fba9edcd187a12f3b7d9c92bb9421864310110a1
Nocnica Mellifera <noctnica@gmail.com>
2023-02-05 17:26:47 -0800

    add comments

--- week1pt1.sql
+++ week1pt1.sql
@@ -1,15 +1,22 @@
+/*Nočnica Mellifera
+Advanced SQL Project 1 */
+
 --only runs on Snowflake, since it uses qualify
 --this code is QUITE untidy
 
+-- cities
 with cities as (
 select 
     upper(trim(city_name))as city_name,
     upper(state_abbr) as state,
     GEO_LOCATION,
+    --thought this concat would make my life easier, even money whether it did
     concat(city_name,state) as city_state
 from vk_data.resources.us_cities
 qualify row_number() over (partition by city_name, state order by 1) = 1
 ),
+
+--so proud was when creating this CTE, but then
 customers as (
 select
     c2.first_name,
@@ -23,12 +30,14 @@ FROM vk_data.customers.customer_address as c1
 JOIN vk_data.customers.customer_data as c2
     on c2.customer_id = c1.customer_id
 ),
+--couldn't figure out how to do my inner join right for validation of customers, ended up with this second table, alas
 valid_customers as(
     SELECT * 
     FROM customers
     INNER JOIN cities
     on cities.city_state = customers.city_state
 ),
+--suppliers with geo locations
 suppliers as(
     select
         supplier_id,
@@ -42,6 +51,7 @@ suppliers as(
         upper(s1.supplier_city) = c1.city_name
         AND s1.supplier_state = c1.state
 ),
+--now this is totally clean
 final_result as(
 select 
     customer_id,
@@ -53,6 +63,8 @@ select
     st_distance(geo, geo_location) /1000 as distance_in_km
 from valid_customers 
 CROSS JOIN suppliers 
+--again I don't really 'get' qualify yet, I would have done this step with an intermediary temp table
+--68 and 69 are Amlan's code
     qualify row_number() over (partition by customer_id order by distance_in_km) = 1
         order by customer_last_name, customer_first_name
 )
